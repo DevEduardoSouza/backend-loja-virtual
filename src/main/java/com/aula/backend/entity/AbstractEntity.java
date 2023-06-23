da9cf7b8d6cc90e46c586a0ef7f1289cfd387bc6
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-23 18:30:57 -0300

    Adicionando o AbstractEntity

---
+++ src/main/java/com/aula/backend/entity/AbstractEntity.java
@@ -0,0 +1,20 @@
+package com.aula.backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+public class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP) //Informar que vai ser salvo data e hora
+    private Date DataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date DataAtualizacao;
+}
--- src/main/java/com/aula/backend/entity/Estado.java
+++ src/main/java/com/aula/backend/entity/Estado.java
@@ -9,15 +9,8 @@ import java.util.Date;
 @Entity
 @Table(name = "Estados")
 @Data
-public class Estado {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Estado extends AbstractEntity{
     private String nome;
     private String sigla;
-    @Temporal(TemporalType.TIMESTAMP) //Informar que vai ser salvo data e hora
-    private Date DataCriacao;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date DataAtualizacao;
 
 }
