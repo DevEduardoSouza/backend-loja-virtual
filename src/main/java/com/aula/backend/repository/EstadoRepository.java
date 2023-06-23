bb2a18155c576bcc9f9676baf174b3b4113b73d7
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-23 16:16:31 -0300

    Adicionando um service para estado

---
+++ src/main/java/com/aula/backend/repository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.aula.backend.repository;
+
+
+import com.aula.backend.entity.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
