04f389ff21ff1d3e6c752573340395d29b13d135
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-24 17:23:41 -0300

    Adicionando o controller de produto

---
+++ src/main/java/com/aula/backend/controller/ProdutoController.java
@@ -0,0 +1,38 @@
+package com.aula.backend.controller;
+
+import com.aula.backend.entity.Produto;
+import com.aula.backend.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produto")
+public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping("/")
+    public List<Produto> bucarTodas(){
+        return produtoService.buscarTodos();
+    }
+
+    @PostMapping("/")
+    public Produto inserir(@RequestBody Produto produto){
+        return  produtoService.inserir(produto);
+    }
+
+    @PutMapping("/")
+    public Produto alterar(@RequestBody Produto produto){
+        return produtoService.alterar(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+        produtoService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
