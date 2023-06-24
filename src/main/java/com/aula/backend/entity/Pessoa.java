dfc31740a17247a7682136351e6142ec71ac7de7
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-24 16:48:23 -0300

    Adicionando a entidade pessoa

---
+++ src/main/java/com/aula/backend/entity/Pessoa.java
@@ -0,0 +1,23 @@
+package com.aula.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa extends AbstractEntity{
+    @CPF
+    private String cpf;
+    @Email
+    private String email;
+    private String endereco;
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "idCidade")
+    private Cidade cidade;
+}
