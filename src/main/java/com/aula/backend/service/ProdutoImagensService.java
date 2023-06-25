0d2e8d5eed19a61b54c8da86eb9f82f85e59e607
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-25 11:16:23 -0300

    Adicionando o service de ProdutoImagens

--- src/main/java/com/aula/backend/service/ProdutoImagensService.java
+++ src/main/java/com/aula/backend/service/ProdutoImagensService.java
@@ -2,12 +2,19 @@ package com.aula.backend.service;
 
 
 import com.aula.backend.entity.Categoria;
+import com.aula.backend.entity.Produto;
 import com.aula.backend.entity.ProdutoImagens;
 import com.aula.backend.repository.CategoriaRepository;
 import com.aula.backend.repository.ProdutoImagensRepository;
+import com.aula.backend.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
 
@@ -16,16 +23,42 @@ public class ProdutoImagensService {
     @Autowired
     private ProdutoImagensRepository produtoImagensRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
 
     public List<ProdutoImagens> bucarTodos(){
         return produtoImagensRepository.findAll();
     }
 
 
-    public ProdutoImagens inserir(ProdutoImagens produtoImagens){
-        produtoImagens.setDataCriacao(new Date());
-        ProdutoImagens produtoImagensNovo = produtoImagensRepository.saveAndFlush(produtoImagens);
-       return  produtoImagensNovo;
+    public ProdutoImagens inserir(Long idProduto, MultipartFile file){
+        ProdutoImagens objeto = new ProdutoImagens();
+        Produto produto = produtoRepository.findById(idProduto).get();
+
+        try {
+
+            if(!file.isEmpty()){
+                byte[] bytes = file.getBytes();
+                String nomeImagem = String.valueOf(produto.getId()) + file.getOriginalFilename();
+
+                Path caminho = Paths.get("c:/imagens/" + nomeImagem);
+
+                Files.write(caminho, bytes);
+
+                objeto.setNome(nomeImagem);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+        objeto.setProduto(produto);
+        objeto.setDataCriacao(new Date());
+        objeto = produtoImagensRepository.saveAndFlush(objeto);
+        return  objeto;
     }
 
     public ProdutoImagens alterar(ProdutoImagens produtoImagens){
