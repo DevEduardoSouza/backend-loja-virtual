b57d7454e5e979077a9197fe5288fd7016a9a06e
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-25 09:35:11 -0300

    Adicionando @Data em permissao

--- src/main/java/com/aula/backend/entity/Permissao.java
+++ src/main/java/com/aula/backend/entity/Permissao.java
@@ -2,8 +2,10 @@ package com.aula.backend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "permissao")
+@Data
 public class Permissao  extends AbstractEntity{
 }
