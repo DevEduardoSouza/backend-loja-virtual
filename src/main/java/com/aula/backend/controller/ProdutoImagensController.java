386610149dd1d36c8fdb26967e3b6d58e19c0c0e
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-25 11:43:12 -0300

    Adicionando o controller de ProdutoImagens

---
+++ src/main/java/com/aula/backend/controller/ProdutoImagensController.java
@@ -0,0 +1,40 @@
+package com.aula.backend.controller;
+
+
+import com.aula.backend.entity.Categoria;
+import com.aula.backend.entity.ProdutoImagens;
+import com.aula.backend.service.CategoriaService;
+import com.aula.backend.service.ProdutoImagensService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtoImagens")
+public class ProdutoImagensController {
+
+    @Autowired
+    private ProdutoImagensService produtoImagensService;
+
+    @GetMapping("/")
+    public List<ProdutoImagens> bucarTodas(){
+        return produtoImagensService.bucarTodos();
+    }
+
+    @PostMapping("/")
+    public ProdutoImagens inserir(@RequestParam("idProduto") Long idProduto, @RequestParam("file") MultipartFile file){
+        return produtoImagensService.inserir(idProduto, file);
+    }
+    @PutMapping("/")
+    public ProdutoImagens alterar(@RequestBody ProdutoImagens produtoImagens){
+        return produtoImagensService.alterar(produtoImagens);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id")Long id){
+        produtoImagensService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+}
